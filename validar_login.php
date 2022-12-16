ab3e2e441c17efdbd36b282de38eff9fcb79c861
Christophre Nolan MB <christopher.nolanmb@gmail.com>
2022-12-15 23:36:05 -0600

    mod "validar_login"

--- validar_login.php
+++ validar_login.php
@@ -1,31 +1,27 @@
 <?php
-    include 'db_conn.php';
+include 'db_conn.php';
 
-    $matricula = $_POST['matricula'];
-    $password = $_POST['contrasenia'];
-    
-    
-    $validar_login = mysqli_query($conexion, "SELECT * FROM alumno WHERE matricula = '$matricula' AND Password = '$password'");
+$usuario = $_POST['matricula'];
+$password = $_POST['contrasenia'];
 
-    if(mysqli_num_rows($validar_login) > 0) {
-        echo '
+
+$validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '$usuario' AND password = '$password'");
+
+if (mysqli_num_rows($validar_login) > 0) {
+    echo '
             <script>
-                alert("Usuario encontrado!");
+                // alert("Usuario encontrado!");
             </script>
         ';
-
-         header("Location: calificaciones.html"); 
-        exit;
-        
-    }else {
-        echo "
+    header("Location: calificaciones.html");
+    exit;
+} else {
+    echo "
             <script> 
                 alert('Usuario no encontrado');
-                
+                window.location = 'login.html';
             </script>
-            
         ";
-        header('login.php');
-            exit;
-
-    }
+    #header('Location : login.html');
+    exit;
+}
