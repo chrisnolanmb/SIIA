f7e2087cc3c53b3f7260cf80b49df8efe80f2540
Christophre Nolan MB <christopher.nolanmb@gmail.com>
2022-12-18 11:14:46 -0600

    agregada la imagen en datos personales

--- datosPersonales.php
+++ datosPersonales.php
@@ -1,15 +1,15 @@
 <?php
 include 'db_conn.php';
 session_start();
-if(!isset($_SESSION['usuario_logged'])) {
-    echo '
+if (!isset($_SESSION['usuario_logged'])) {
+  echo '
       <script> 
         alert("You are not logged!");
       </script>
     ';
-    header("location:login.php");
-    session_destroy();
-    die();
+  header("location:login.php");
+  session_destroy();
+  die();
 }
 $user = $_SESSION['usuario_logged'];
 //echo $user;
@@ -47,11 +47,11 @@ FROM
 
 $result_query_datos_personales = mysqli_query($conexion, $query_datos_personales) or die("Bad query: $query_datos_personales");
 
-$result_query_name = mysqli_query($conexion,$quer_name) or die("Bad query: $quer_name");
+$result_query_name = mysqli_query($conexion, $quer_name) or die("Bad query: $quer_name");
 
 // session_destroy();
 
-while($row = mysqli_fetch_assoc($result_query_name)){
+while ($row = mysqli_fetch_assoc($result_query_name)) {
   $nombre = $row['Nombre'];
 }
 
@@ -62,109 +62,116 @@ while($row = mysqli_fetch_assoc($result_query_name)){
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
-    <meta name="theme-color" content="#30683B"/>
-    <link rel="stylesheet" href="styles/main.css">
-    <title>siia</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
+  <meta name="theme-color" content="#30683B" />
+  <link rel="stylesheet" href="styles/main.css">
+  <title>siia</title>
 </head>
+
 <body style="background-color: #e1e1e1;">
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg  navbar-dark" style=" font-family: 'Poppins', sans-serif; position: sticky; top: 0px; padding: 0px;" >
-        <div class="container-fluid" style="background-color: #30683B; border-radius: 0px 0px 20px 20px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
-            <a class="navbar-brand" href="calificaciones.php">
-                <img src="img/esiiaSinSombra.png" alt="Bootstrap" width="120">
-              </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="border: none;">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="calificaciones.php">Calificaciones</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="reinscripcion.php">Reinscripción</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#"><b>Datos personales</b></a>
-              </li>
-              <li class="nav-item">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg  navbar-dark" style=" font-family: 'Poppins', sans-serif; position: sticky; top: 0px; padding: 0px;">
+    <div class="container-fluid" style="background-color: #30683B; border-radius: 0px 0px 20px 20px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);">
+      <a class="navbar-brand" href="calificaciones.php">
+        <img src="img/esiiaSinSombra.png" alt="Bootstrap" width="120">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="border: none;">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="calificaciones.php">Calificaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="reinscripcion.php">Reinscripción</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#"><b>Datos personales</b></a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link active" href="cerrarSesion.php">Cerrar sesion</a>
           </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#"><br></a>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
-      </nav>
+          <li class="nav-item">
+            <a class="nav-link active" href="#"><br></a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
 
 
-      <!-- Grid principal -->
-      <div class="container" style="font-family: 'Poppins', sans-serif;">
+  <!-- Grid principal -->
+  <div class="container" style="font-family: 'Poppins', sans-serif;">
 
-        <div class="row justify-content-m-center" style="color: rgba(0, 0, 0, 0.8);">
+    <div class="row justify-content-m-center" style="color: rgba(0, 0, 0, 0.8);">
 
-          <div class="col-sm-3">
-            <br>
-            <h3 style="color: rgba(0, 0, 0, 1.0);">
-                Datos personales
-            </h3>
-            Lic. En Ciencias Físico Matemáticas
-          </div>
-
-          <div class="col-sm-3 text-center">
-            
-          </div>
-
-          <div class="col-sm-3 text-center">
-            <div class="circulo ">
-
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="image"/>
-        <input type="submit" name="submit_image" value="UPLOAD"/>
-    </form>
-              <!-- <img src="data:iamge/jpg:bsae64 img/student.png" alt="" style="border-radius: 100%;"> -->
-              <img src='vista.php' alt='Img blob desde MySQL' width="600" /> 
-              
-                           
-            </div>
-            <?php echo $nombre?> <br>
-            <?php echo $user ?>
-          </div>
-
-        </div>
+      <div class="col-sm-3">
         <br>
+        <h3 style="color: rgba(0, 0, 0, 1.0);">
+          Datos personales
+        </h3>
+        Lic. En Ciencias Físico Matemáticas
+      </div>
 
-        
+      <div class="col-sm-3 text-center">
 
-        <!-- Seleción de materias -->
-        <div class="container">
-            <div class="row ">
-                <div class="col-sm-6 ">
-                    <div class="recuadro">
-                        <table class="table">
-                            <thead>
-                              <tr>
-                                <th scope="col">Asignatura</th>
-                                <th scope="col">Profesor</th>
-                                <th scope="col">Créditos</th>
-                                <th scope="col">Ciclo</th>
+      </div>
+
+      <div class="col-sm-3 text-center">
+        <div class="circulo ">
+
+          <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="image" />
+            <input type="submit" name="submit_image" value="UPLOAD" />
+          </form>
+          <?php
+          include("db_conn.php");
+          $user = $_SESSION['usuario_logged'];
+          $query_image = "SELECT image FROM alumno WHERE matricula = '$user'";
+          $result_image = $conexion->query($query_image) or die("Bad query: $query_image");
+          while ($row = $result_image->fetch_assoc()) {
+          ?>
+            <img src="data:image/jpeg;base64,<?php echo base64_encode($row['image']); ?>" style="border-radius: 100%;">
+          <?php
+          }
+          ?>
+        </div>
+        <?php echo $nombre ?> <br>
+        <?php echo $user ?>
+      </div>
+    </div>
+    <br>
+    <!-- Seleción de materias -->
+    <div class="container">
+      <div class="row ">
+        <div class="col-sm-6 ">
+          <div class="recuadro">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Asignatura</th>
+                  <th scope="col">Profesor</th>
+                  <th scope="col">Créditos</th>
+                  <th scope="col">Ciclo</th>
 
 
-                                
-                              </tr>
-                            </thead>
-                            <tbody>
-                            <?php while($row = mysqli_fetch_object($result_query_datos_personales)){
-                                        echo "
+
+                </tr>
+              </thead>
+              <tbody>
+                <?php while ($row = mysqli_fetch_object($result_query_datos_personales)) {
+                  echo "
                                         <tr>
                                           <td>$row->Asignatura</td>
                                           <td>$row->Profesor</td>
@@ -172,15 +179,15 @@ while($row = mysqli_fetch_assoc($result_query_name)){
                                           <td>$row->Ciclo</td>
                                           
                                         </tr>";
-}
-                            ?>
+                }
+                ?>
 
 
 
 
 
 
-                              <!-- <tr>
+                <!-- <tr>
                                 <td scope="row">Física Moderna</td>
                                 <td>10</td>
                               </tr>
@@ -188,71 +195,72 @@ while($row = mysqli_fetch_assoc($result_query_name)){
                                 <td scope="row">Cálculo Complejo</td>
                                 <td>6</td>
                               </tr> -->
-                              
-                            </tbody>
-                        </table>
 
-                        
-
-                    </div>
-                    
-                </div>
+              </tbody>
+            </table>
 
 
-                <div class="col-sm-6" class="recuadro" style="background-color: none;">
-                  <div class="row justify-content-md-center" >
-                    <div class="accordion" id="accordionExample" >
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="headingOne" >
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                              Certificado de bachillerato
-                            </button>
-                          </h2>
-                          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <a href="">Descargar certificado de bachillerato</a>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="headingTwo">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                              Constancia de estudios
-                            </button>
-                          </h2>
-                          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                              Realiza el pago para solicitar constancia de estudios en <b>Control Escolar</b>
-                              <button class="enviarActivado">Relizar pago</button>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="accordion-item">
-                          <h2 class="accordion-header" id="headingThree">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                              Credencial estudiantil 
-                            </button>
-                          </h2>
-                          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                              <a href="">Credencial escaneada (inválida sin sellos)</a> <br>
-                              Realizar pago para solicitar resposición de credencial física
-                            </div>
-                          </div>
-                        </div>
-                    </div>
+
+          </div>
+
+        </div>
+
+
+        <div class="col-sm-6" class="recuadro" style="background-color: none;">
+          <div class="row justify-content-md-center">
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Certificado de bachillerato
+                  </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <a href="">Descargar certificado de bachillerato</a>
+                  </div>
                 </div>
               </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Constancia de estudios
+                  </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    Realiza el pago para solicitar constancia de estudios en <b>Control Escolar</b>
+                    <button class="enviarActivado">Relizar pago</button>
+                  </div>
                 </div>
-                
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Credencial estudiantil
+                  </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    <a href="">Credencial escaneada (inválida sin sellos)</a> <br>
+                    Realizar pago para solicitar resposición de credencial física
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-        
-                
+      </div>
 
-        
+    </div>
+  </div>
 
-      <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+
+
+
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
+
 </html>
